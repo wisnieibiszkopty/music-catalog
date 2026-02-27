2c40741f354c8a882b51e64af54ec0173abc2e67
Kamil Wodowski <kamilwodowski@o2.pl>
2026-02-27 17:01:51 +0100

    setup project structure

---
+++ src/services/Catalog.Service/Program.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.Run();
