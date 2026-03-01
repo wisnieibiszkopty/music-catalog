cf488d0634fb86e7c1ed9640f4136eff72b87b60
Kamil Wodowski <kamilwodowski@o2.pl>
2026-03-01 14:27:08 +0100

    add nuxt ui

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ui from '@nuxt/ui/vue-plugin'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ui)
 
 app.mount('#app')
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    ui()
   ],
   resolve: {
     alias: {
