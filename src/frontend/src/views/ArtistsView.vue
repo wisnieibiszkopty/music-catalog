0ce71b7de93c4d89ecb6ad9cc608b7bad4f3ec35
Kamil Wodowski <kamilwodowski@o2.pl>
2026-03-03 08:37:44 +0100

    finish keycloak auth on frontend

--- src/frontend/src/views/ArtistsView.vue
+++ src/frontend/src/views/ArtistsView.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import keycloak from '@/services/keycloak';
-
-function login(){
-  keycloak.login({
-    redirectUri: window.location.origin
-  })
-}
 
 </script>
 
 <template>
-<p>artists</p>
-
-  <UButton @click="login" icon="i-lucide-rocket" size="md" color="primary" variant="solid">Login</UButton>
-
+  <p>artists</p>
 </template>
 
 <style scoped>
--- src/frontend/src/views/LayoutView.vue
+++ src/frontend/src/views/LayoutView.vue
@@ -1,38 +1,10 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
 
-const items: NavigationItems[] = [
-  {
-    label: 'Login',
-    active: true,
-    to: '/login'
-  },
-  {
-    label: 'Register',
-    active: true,
-    to: '/register'
-  },
-  {
-    label: 'Artists',
-    active: true,
-    to: '/artists'
-  },
-  {
-    label: 'Albums',
-    active: true,
-    to: '/albums'
-  }
-];
 </script>
 
 <template>
   <UDashboardGroup>
-    <UDashboardSidebar resizable>
-      <UNavigationMenu
-        :items="items"
-        orientation="vertical"
-      />
-    </UDashboardSidebar>
+    <Sidebar/>
 
     <UDashboardPanel>
       <template #header>
